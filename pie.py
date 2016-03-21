79cb65cf3d0a16492013962351254eaf6496fcb4
Adam Kerz <github@kerz.id.au>
2016-03-21 15:37:14 +1000

    Adding new command line options, adding parsing for task arguments, fixing a bug because running python pie.py doesn't actually import pie as a module (so @task in pie_tasks.py will set a different pie.tasks module variable to what is used in pie.main()), and comment tidy up

--- pie.py
+++ pie.py
@@ -6,6 +6,7 @@ Great for bootstrapping a development environment, and then interacting with it.
 __VERSION__='0.0.1'
 
 import os
+import re
 import subprocess
 import sys
 from functools import wraps
@@ -15,46 +16,49 @@ WINDOWS=(os.name=='nt')
 PY3=(sys.version_info>=(3,0))
 
 
-"""
-A class that can be used like a dictionary with more succinct syntax:
-    l=Lookup(name='example',value='good')
-    print(l.name)
-    l.newValue=2
-"""
+# ----------------------------------------
+# configuration
+# ----------------------------------------
 class Lookup(object):
+    """
+    A class that can be used like a dictionary with more succinct syntax:
+        l=Lookup(name='example',value='good')
+        print(l.name)
+        l.newValue=2
+    """
     def __init__(self,**entries):
         self.__dict__.update(entries)
 
 
-"""
-options is a lookup object where predefined options (in code) can be placed, as well as provided on the command line.
-"""
+# options is a lookup object where predefined options (in code) can be placed, as well as provided on the command line.
 options=Lookup()
 
 
-"""
-context is used to keep track of what context a command is being executed within.
-    with venv('venv/build'):
-        cmd('python -m pip')
-"""
+# context is used to keep track of what context a command is being executed within.
+#     with venv('venv/build'):
+#         cmd('python -m pip')
 context=Lookup()
 
 
-"""
-tasks is a dictionary of registered tasks, where key=name. Tasks are possibly from within submodules where name=module.task.
-"""
+
+# ----------------------------------------
+# tasks
+# ----------------------------------------
+# tasks is a dictionary of registered tasks, where key=name. Tasks are possibly from within submodules where name=module.task.
 tasks={}
 
-
-"""
-A decorator that converts a simple Python function into a pie task.
- - parameters is a list of objects (use Lookup) with the following attributes:
-     name - name of the param
-     type - descriptive type of the param
-     conversionFn - a function that will take a string and convert it to the desired type
-"""
 def task(parameters=[]):
+    """
+    A (function that returns a) decorator that converts a simple Python function into a pie task.
+     - parameters is a list of objects (use Lookup) with the following attributes:
+         name - name of the param
+         type - descriptive type of the param
+         conversionFn - a function that will take a string and convert it to the desired type
+    """
     def decorator(taskFn):
+        # register the task
+        tasks[taskFn.__name__]=taskFn
+        # then wrap the function
         @wraps(taskFn)
         def wrapper(*args,**kwargs):
             # go through parameters and make sure they're all there, otherwise inject or prompt for them
@@ -64,24 +68,28 @@ def task(parameters=[]):
     return decorator
 
 
-"""
-Executes a system command
-"""
+
+# ----------------------------------------
+# operations
+# ----------------------------------------
 def cmd(c):
+    """
+    Executes a system command
+    """
     subprocess.call(c,shell=True)
 
 
-"""
-Runs a pip command
-"""
 def pip(c):
+    """
+    Runs a pip command
+    """
     cmd('python -m pip {}'.format(c))
 
 
-"""
-A context class used to execute commands within a virtualenv
-"""
 class venv(object):
+    """
+    A context class used to execute commands within a virtualenv
+    """
     def __init__(self,path):
         self.path=path
 
@@ -97,27 +105,72 @@ class venv(object):
         else:
             raise Exception('I dunno')# TODO
 
-class Argument(object):
-    def __init__(self,type):
-        self.type=type
 
+
+# ----------------------------------------
+# Command line functionality
+# ----------------------------------------
+class Argument(object):
     def execute(self):
-        raise NotImplemented()
+        print(str(self))
+        # raise NotImplemented()
+
+
+class Version(Argument):
+    def execute(self):
+        print('pie v{}'.format(__VERSION__))
+
+    def __str__(self):
+        return 'Version: {}'.format(__VERSION__)
+    __repr__=__str__
+
+
+class CreateBatchFile(Argument):
+    def execute(self):
+        if WINDOWS:
+            with open('pie.bat','w') as fout:
+                fout.write('@echo off\npython -c "import pie; pie.main()" %*\n')
+        else:
+            with open('pie','w') as fout:
+                fout.write('python -c "import pie; pie.main()" %*\n')
+
+    def __str__(self):
+        return 'CreateBatchFile'
+    __repr__=__str__
+
+
+class Help(Argument):
+    def execute(self):
+        print('Usage:  pie -v | -h | -b | {-o name=value | task[(args...)]}')
+        print('Version: v{}'.format(__VERSION__))
+        print('')
+        print('  -v    Display version')
+        print('  -h    Display this help')
+        print('  -b    Create batch file shortcut')
+        print('  -o    Sets an option with name to value')
+        print('  task  Runs a task passing through arguments if required')
+
+    def __str__(self):
+        return 'Help'
+    __repr__=__str__
 
 
 class Option(Argument):
     def __init__(self,name,value):
-        super(Option,self).__init__('option')
         self.name=name
         self.value=value
 
     def execute(self):
-        options[self.name]=self.value
+        setattr(options,self.name,self.value)
+
+
+    def __str__(self):
+        return 'Option: {}={}'.format(self.name,self.value)
+    __repr__=__str__
 
 
 class Task(Argument):
     def __init__(self,name,args=[],kwargs={}):
-        super(Option,self).__init__('task')
         self.name=name
         self.args=args
         self.kwargs=kwargs
@@ -126,34 +179,60 @@ class Task(Argument):
         # TODO: check task arg requirements and prompt - OR - can this be done by the @task decorator?
         tasks[self.name](*self.args,**self.kwargs)
 
+    def __str__(self):
+        return 'Task: {}(args={},kwargs={})'.format(self.name,self.args,self.kwargs)
+    __repr__=__str__
 
+
+
+# ----------------------------------------
+# Command line parsing
+# ----------------------------------------
+TASK_RE=re.compile(r'(?P<name>[^()]+)(\((?P<args>.*)\))?')
 def parseArguments(args):
     # skip the name of the command
     i=1
     parsed=[]
     while i<len(args):
         arg=args[i]
-        if arg=='-o':
+        if arg=='-v':
+            parsed.append(Version())
+        elif arg=='-h':
+            parsed.append(Help())
+        elif arg=='-b':
+            parsed.append(CreateBatchFile())
+        elif arg=='-o':
             name,value=args[i+1].split('=')
             parsed.append(Option(name,value))
             i+=1
         else:
-            parsed.append(Task(arg))
+            mo=TASK_RE.match(arg)
+            if mo:
+                args=mo.group('args')
+                args=args.split(',') if args else []
+                # TODO: add further parsing to handle keyword arguments
+                parsed.append(Task(mo.group('name'),args=args,kwargs={}))
+            else:
+                raise Exception('Unknown task format: {}'.format(arg))
         i+=1
     return parsed
 
 
-def main(args):
-    print(parseArguments(args))
+
+# ----------------------------------------
+# entry point
+# ----------------------------------------
+def main():
+    args=parseArguments(sys.argv)
+    if args:
+        import pie_tasks
+        for a in args:
+            a.execute()
+    else:
+        Help().execute()
 
 
 if __name__=='__main__':
-    main(sys.argv)
-
-    if WINDOWS:
-        with open('pie.bat','w') as fout:
-            fout.write('@echo off\npython -m pie %*\n')
-    else:
-        with open('pie','w') as fout:
-            fout.write('python -m pie %*\n')
-
+    # import pie so that both we and any pie_tasks code that imports pie are referring to the same module variables
+    import pie
+    pie.main()
