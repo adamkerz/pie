5808efd96fdabc905e032045a0ee737c4f2e71bc
Adam Kerz <github@kerz.id.au>
2016-03-23 16:33:38 +1000

    Some sample tasks to do ad_hoc testing whilst doing initial research and development

--- pie_tasks.py
+++ pie_tasks.py
@@ -39,3 +39,25 @@ def a_long_long_long_long_long_long_task_name():
     And a super duper long (but repetitive) description too.
     """
     print('long')
+
+
+@task(parameters=[dict(name='v',conversionFn=int)])
+def params(v):
+    """Test param prompting and injection."""
+    print('sum: {}'.format(v+2))
+
+
+@task
+def testCmd():
+    cmd('echo hi')
+
+
+def not_a_task():
+    pass
+
+
+@task
+def useVenv():
+    with venv('venv'):
+        # cmd('python')
+        pip('install humanize==0.5.1')
