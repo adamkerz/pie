aa71b3073fd07820ae34ae6d284acdc489fa2f02
Adam Kerz <github@kerz.id.au>
2016-03-24 12:01:36 +1000

    Refactoring task decorator to handle discovery of tasks in modules and adding functions to do that

--- pie.py
+++ pie.py
@@ -5,11 +5,16 @@ Great for bootstrapping a development environment, and then interacting with it.
 """
 __VERSION__='0.0.1'
 
+import inspect
 import os
 import re
 import subprocess
 import sys
+import types
 from functools import wraps
+
+
+__all__=['task','cmd','pip','venv']
 
 
 # environment constants
@@ -52,35 +57,42 @@ context=[]
 # tasks is a dictionary of registered tasks, where key=name. Tasks are possibly from within submodules where name=module.task.
 tasks={}
 
+
+class TaskWrapper(object):
+    """
+    A callable wrapper around a task function. Provides prompting for missing function arguments.
+    """
+    def __init__(self,fn,params):
+        self.fn=fn
+        self.params=params
+        self.desc=fn.__doc__
+
+    def __call__(self,*args,**kwargs):
+        # args might be a tuple, but we want to append to it
+        args=list(args)
+        # go through parameters and make sure we have arguments for each, otherwise inject or prompt for them
+        for i,p in enumerate(self.params):
+            if len(args)<=i:
+                # TODO: use a default value if provided - would be best to use the default value as provided with the function definition, rather than add a 'default' key in parameters dicts.
+                # prompt for a missing argument
+                promptStr=p['prompt'] if 'prompt' in p else 'Please enter a value for {}: '.format(p['name'])
+                v=PROMPT_FN(promptStr)
+                args.append(v)
+            # and apply the conversionFn
+            if 'conversionFn' in p: args[i]=p['conversionFn'](args[i])
+        return self.fn(*args,**kwargs)
+
+
 def task(parameters=[]):
     """
-    A (function that returns a) decorator that converts a simple Python function into a pie task.
+    A (function that returns a) decorator that converts a simple Python function into a pie Task.
      - parameters is a list of objects (use Lookup) with the following attributes:
-         name - name of the param
-         type - descriptive type of the param
+         name - name of the parameter
          conversionFn - a function that will take a string and convert it to the desired type
     """
     def decorator(taskFn):
-        # wrap the function
-        @wraps(taskFn)
-        def wrapper(*args,**kwargs):
-            # args might be a tuple, but we want to append to it
-            args=list(args)
-            # go through parameters and make sure they're all there, otherwise inject or prompt for them
-            for i,p in enumerate(parameters):
-                if len(args)<=i:
-                    # TODO: use a default value if provided - would be best to use the default value as provided with the function definition, rather than add a 'default' key in parameters dicts.
-                    # prompt for a missing value
-                    promptStr=p['prompt'] if 'prompt' in p else 'Please enter a value for {}: '.format(p['name'])
-                    v=PROMPT_FN(promptStr)
-                    args.append(v)
-                # and apply the conversionFn
-                if 'conversionFn' in p: args[i]=p['conversionFn'](args[i])
-            return taskFn(*args,**kwargs)
-
-        # then register the task
-        tasks[taskFn.__name__]={'fn':wrapper,'desc':taskFn.__doc__,'params':parameters}
-        return wrapper
+        # convert the function into a callable task instance
+        return TaskWrapper(taskFn,parameters)
 
     # check in case we were called as a decorator eg. @task (without the function call)
     if callable(parameters):
@@ -92,6 +104,25 @@ def task(parameters=[]):
 
     # otherwise return the decorator function
     return decorator
+
+
+def registerTasksInModule(modulePath,module):
+    """Recursively traverse through modules, registering tasks"""
+    modulePrefix=(modulePath+'.' if modulePath else '')
+    for k,v in vars(module).items():
+        if isinstance(v,TaskWrapper):
+            tasks[modulePrefix+k]=v
+        elif isinstance(v,types.ModuleType):
+            # there is a situation where a module in a package that is imported into another module (so, doubly nested tasks) will also show up at the package level, even though it's not imported there
+            # eg. pie_tasks/__init__.py does "from . import submodule", submodule does "from . import doubleNest", but this also causes doubleNest to show up in pie_tasks
+            # I can't figure out any way to distinguish the difference, but, at the moment, I think it's an obscure enough situation to not worry about yet
+            registerTasksInModule(modulePrefix+k,v)
+
+
+def importTasks(moduleName='pie_tasks'):
+    """Import the pie_tasks module and register all tasks found"""
+    m=__import__(moduleName)
+    registerTasksInModule('',m)
 
 
 
@@ -177,7 +208,7 @@ class ListTasks(Argument):
         for k in sorted(tasks.keys()):
             v=tasks[k]
             if self.includeDescription:
-                desc=v['desc'] or ''
+                desc=v.desc or ''
                 print('{:30} {:.70}'.format(k,desc.replace('\n',' ')))
             else:
                 print(k)
@@ -212,14 +243,14 @@ class Option(Argument):
         return 'Option: {}={}'.format(self.name,self.value)
 
 
-class Task(Argument):
+class TaskCall(Argument):
     def __init__(self,name,args=[],kwargs={}):
         self.name=name
         self.args=args
         self.kwargs=kwargs
 
     def execute(self):
-        tasks[self.name]['fn'](*self.args,**self.kwargs)
+        tasks[self.name](*self.args,**self.kwargs)
 
     def __repr__(self):
         return 'Task: {}(args={},kwargs={})'.format(self.name,self.args,self.kwargs)
@@ -260,7 +291,7 @@ def parseArguments(args):
                 args=mo.group('args')
                 args=args.split(',') if args else []
                 # TODO: add further parsing to handle keyword arguments
-                parsed.append(Task(mo.group('name'),args=args,kwargs={}))
+                parsed.append(TaskCall(mo.group('name'),args=args,kwargs={}))
             else:
                 raise Exception('Unknown task format: {}'.format(arg))
         i+=1
@@ -274,7 +305,7 @@ def parseArguments(args):
 def main():
     args=parseArguments(sys.argv)
     if args:
-        import pie_tasks
+        importTasks()
         for a in args:
             a.execute()
             # print(repr(a))
