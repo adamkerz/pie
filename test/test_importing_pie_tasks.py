9db04a4c51205aff1e4b4ecf338c7e3792e7449c
Adam Kerz <github@kerz.id.au>
2016-03-30 11:00:40 +1000

    Re-writing the tests to allow more modular testing and getting around issues of pie and pie_tasks being in the path and even already imported when running tests

--- test/test_importing_pie_tasks.py
+++ test/test_importing_pie_tasks.py
@@ -1,5 +1,6 @@
 import shutil
 import subprocess
+import sys
 from pathlib import Path
 
 import pytest
@@ -8,16 +9,54 @@ import pytest
 dataPath=Path(__file__).parent/'data'
 
 
+@pytest.fixture(scope='function')
+def pie():
+    """Makes sure pie is freshly imported from cwd, then removes cwd from the path to allow pie_tasks to be imported from wherever we choose"""
+    cwd=str(Path.cwd())
+    if 'pie' in sys.modules: del sys.modules['pie']
+    sys.path.insert(0,cwd)
+    import pie
+    while True:
+        try:
+            sys.path.remove(cwd)
+        except ValueError:
+            break
+    return pie
 
-def test_module_tasks():
-    testPath=dataPath/'module_tasks'
-    shutil.copy('pie.py',str(testPath))
-    o=subprocess.check_output(r'cd {} && python pie.py -l'.format(testPath),shell=True)
-    print(o)
-    assert o!=''
 
-    # how to catch error when running the command
-    # try:
-        # o=subprocess.check_output(r'cd {} && python pie.py -l'.format(testPath),shell=True)
-    # except subprocess.CalledProcessError as e:
-        # print(e.returncode,e.cmd,e.output)
+@pytest.fixture(scope='function')
+def pie_tasks_path(request):
+    """Makes sure pie_tasks has not been imported, and prepends the requested path to sys.path."""
+    p=request.param
+    sys.path.insert(0,str(p))
+    if 'pie_tasks' in sys.modules: del sys.modules['pie_tasks']
+    def fin():
+        """Ensure the requested path is removed from sys.path"""
+        while True:
+            try:
+                sys.path.remove(str(p))
+            except ValueError:
+                break
+    request.addfinalizer(fin)
+    return p
+
+
+@pytest.mark.parametrize('pie_tasks_path',[dataPath/'no_pie_tasks',dataPath/'package_tasks',dataPath/'module_tasks'],indirect=['pie_tasks_path'])
+def test_help(pie,capsys,pie_tasks_path):
+    pie.main(['-h'])
+    out,err=capsys.readouterr()
+    assert out.startswith('Usage')
+
+
+@pytest.mark.parametrize('pie_tasks_path',[dataPath/'no_pie_tasks'])
+def test_list_no_pie_tasks(pie,capsys,pie_tasks_path):
+    pie.main(['-l'])
+    out,err=capsys.readouterr()
+    assert out.startswith('pie_tasks could not be found.')
+
+
+@pytest.mark.parametrize('pie_tasks_path',[dataPath/'package_tasks',dataPath/'module_tasks'],indirect=['pie_tasks_path'])
+def test_list(pie,capsys,pie_tasks_path):
+    pie.main(['-l'])
+    out,err=capsys.readouterr()
+    assert not out.startswith('Usage')
