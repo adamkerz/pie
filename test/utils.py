233c53a8ac059d1b0f7bcddce254b1051527f2c4
Adam Kerz <github@kerz.id.au>
2016-04-01 22:28:42 +1000

    Running pytest as a module and fixing a bug with the tests that arose

--- test/utils.py
+++ test/utils.py
@@ -10,24 +10,26 @@ dataPath=Path(__file__).parent/'data'
 @pytest.fixture(scope='function')
 def pie():
     """Makes sure pie is freshly imported from cwd, then removes cwd from the path to allow pie_tasks to be imported from wherever we choose"""
-    cwd=str(Path.cwd())
     if 'pie' in sys.modules: del sys.modules['pie']
+    cwd=str(Path.cwd())
     sys.path.insert(0,cwd)
     import pie
-    while True:
-        try:
-            sys.path.remove(cwd)
-        except ValueError:
-            break
+    # remove any common references to the cwd (which has a pie_tasks.py file)
+    for p in ('','.',cwd):
+        while True:
+            try:
+                sys.path.remove(p)
+            except ValueError:
+                break
     return pie
 
 
 @pytest.fixture(scope='function')
 def pie_tasks_path(request):
     """Makes sure pie_tasks has not been imported, and prepends the requested path to sys.path."""
+    if 'pie_tasks' in sys.modules: del sys.modules['pie_tasks']
     p=request.param
     sys.path.insert(0,str(p))
-    if 'pie_tasks' in sys.modules: del sys.modules['pie_tasks']
     def fin():
         """Ensure the requested path is removed from sys.path"""
         while True:
